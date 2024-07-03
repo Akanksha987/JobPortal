7d5fccfb2ea41df9c6c223f15d489a64b62feb2c
Akanksha987 <agrawalakanksha23123@gmail.com>
2024-07-03 14:12:00 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,26 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from './pages/NotFound';
+import Home from "./pages/Home";
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './theme';
 
-function App() {
-  return (
-    <>
-      Job Portal
-    </>
-  );
+
+const App = () => {
+
+    return (
+        <>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </>
+    )
 }
 
-export default App;
+export default App
